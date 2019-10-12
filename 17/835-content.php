da3049cb5c49c9ee86b9e4dec03a5ef272748319
nguyenhoa99sz <34055262+nguyenhoa99sz@users.noreply.github.com>
2019-10-12 09:07:59 +0700

    Add files via upload

---
+++ 17/835-content.php
@@ -0,0 +1,54 @@
+<?php
+$url_host = $_SERVER['HTTP_HOST'];
+
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+
+$url_path = $url_host . $matches[1][0];
+
+$url_path = str_replace('\\', '/', $url_path);
+?>
+<div class="type-835">
+<div class="header">
+  <h2>Blog Name</h2>
+</div>
+
+<div class="row">
+  <div class="leftcolumn">
+    <div class="card">
+      <h2>TITLE HEADING</h2>
+      <h5>Title description, Dec 7, 2017</h5>
+      <div class="fakeimg" style="height:200px;">Image</div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    </div>
+    <div class="card">
+      <h2>TITLE HEADING</h2>
+      <h5>Title description, Sep 2, 2017</h5>
+      <div class="fakeimg" style="height:200px;">Image</div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    </div>
+  </div>
+  <div class="rightcolumn">
+    <div class="card">
+      <h2>About Me</h2>
+      <div class="fakeimg" style="height:100px;">Image</div>
+      <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+    </div>
+    <div class="card">
+      <h3>Popular Post</h3>
+      <div class="fakeimg">Image</div><br>
+      <div class="fakeimg">Image</div><br>
+      <div class="fakeimg">Image</div>
+    </div>
+    <div class="card">
+      <h3>Follow Me</h3>
+      <p>Some text..</p>
+    </div>
+  </div>
+</div>
+</div>
---
+++ 21/4-content.php
@@ -0,0 +1,21 @@
+<div class="type-4">
+    <div id="mySidenav" class="sidenav">
+    <div class="nav-heading">
+        <h3 class="nav-title">SIGN IN</h3>
+          <!-- <a href="#" class="closebtn">CLOSE<span>&times;</span></a> -->
+        <a href="#" class="closebtn">CLOSE</a>
+        <div class="btn-close-bar">
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
+    </div>
+      <!-- item -->
+      <div class="product-custom">
+    </div>
+    </div>
+    <div class="website-content">
+        <span id="opennav">Click open</span>
+    </div>
+    <div class="close-side"></div>
+    
+</div>
