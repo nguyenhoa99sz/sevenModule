5373b6d3fb2d5faff379b5fcd1503c54821da019
nguyenhoa99sz <34055262+nguyenhoa99sz@users.noreply.github.com>
2019-10-18 00:26:53 +0700

    Add files via upload

--- 21/4-content.php
+++ 21/4-content.php
@@ -11,6 +11,37 @@
     </div>
       <!-- item -->
       <div class="product-custom">
+        <div class="form-group">
+          <p>username or email</p>
+          <input type="email" class="form-control" name="" id="" aria-describedby="emailHelpId" placeholder="">
+          <p>password</p>
+          <input type="email" class="form-control" name="" id="" aria-describedby="emailHelpId" placeholder="">
+          <br>
+          <div class="olol">
+          <button type="email" class="form-control" name="" id="" aria-describedby="emailHelpId" placeholder="">LOGIN</button></div>
+        </div>
+        <div class="login-form-footer" style="font-size:14px; padding: 25px;">
+          <input type="checkbox" name="vehicle1" value="Bike"> I have a bike 
+          <a href="#" style="float:right; padding: 0px;"><i class="fa fa-globe"></i>Forgot your password?</a>
+          <br>
+          <br>
+          <div class="plpl" style="position: relative; text-align:center;">
+        <div style="border-top: 2px solid #ccc; width: 100%; position:absolute; top: 50%;z-index: -1;"></div>
+        <b class="pro" style=" background: #fff; z-index: 9999;">Or login with</b>
+        </div>
+        <br>
+        <button type="button" class="btn btn-primary"><i class="fa fa-facebook-square"></i>Facebook</button>
+        <br>
+        <br>
+        <button type="button" class="btn btn-info"><i class="fa fa-envelope-open"></i>Gmail</button>
+        <br>
+        <br>
+        <div class="kmkm" style="text-align:center;">
+          No Account Yet??
+          <a href="#">Create An Account</a>
+        </div>
+        </div>
+       
     </div>
     </div>
     <div class="website-content">
