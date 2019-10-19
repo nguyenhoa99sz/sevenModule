ebdea9bd90630e42c6f9ff593179d6a9abd57e05
nguyenhoa99sz <nguyennguyenhoa9c@gmail.com>
2019-10-19 10:05:50 +0700

    Revert "commit"
    This reverts commit 84a7bac26b46f8ea79061ade5463915abe413c38.

--- 14/413-content.php
+++ 14/413-content.php
@@ -10,95 +10,45 @@ $url_path = str_replace('\\', '/', $url_path);
 <section id="tabs" class="project-tab">
 <div class="container">
     <div class="row">
-		<div class="col-md-12" >
+		<div class="col-md-12">
 			<h3></h3>
 
-			<div class="tabbable-panel" >
+			<div class="tabbable-panel">
 				<div class="tabbable-line">
 					<ul class="nav nav-tabs ">
 						<li class="active">
 							<a href="#tab_default_1" data-toggle="tab">
-							Description </a>
+							Tab 1 </a>
 						</li>
 						<li>
 							<a href="#tab_default_2" data-toggle="tab">
-							Review (0) </a>
+							Tab 2 </a>
 						</li>
 						<li>
 							<a href="#tab_default_3" data-toggle="tab">
-							Shiping And Delevery </a>
+							Tab 3 </a>
 						</li>
 					</ul>
 					<div class="tab-content">
 						<div class="tab-pane active" id="tab_default_1">
-							<div class="col-md-6">
-								<h1>Viverra a consectetur</h1>
-								<br>
-								Enim in malesuada fusce dolor mi massa leo taciti nulla vestibulum dignissim senectus vitae elit ullamcorper primis.
-								<br>
-								<br>
-								<b>Facilisis scelerisque mi</b> <br> <br>
-								Amet quisque adipiscing scelerisque vestibulum a parturient aptent et pretium a a suspendisse a vestibulum dolor a adipiscing a curabitur integer vulputate praesent convallis. Est nisi ante accumsan pulvinar facilisi orci dignissim lacinia ad sodales conubia viverra a consectetur lectus cras quis aliquam eros curabitur ultricies mauris lectus dictumst magna hac aenean. Odio nec adipiscing. Pulvinar in taciti dui condimentum a vel bibendum aliquet velit nam ultrices. Odio nec adipiscing neque purus parturient a est eu accumsan placerat vestibulum.
-							</div>
-							<div class="col-md-6" style="margin-top : 30px;">
-								<b>Ullamcorper metus
-								</b>
-								<br><br>
-								Nam orci mi non ultricies ut eu in vitae pharetra est in dapibus eu cum nam. Nisl orci quisque congue in ullamcorper metus a hac a arcu tristique class ullamcorper mi lacinia leo id mi at lectus. Pulvinar in taciti dui condimentum a vel bibendum aliquet velit nam ultrices. Odio nec adipiscing neque purus parturient a est eu accumsan placerat dapibus adipiscing parturient placerat a tincidunt enim.
-								<br>
-								<br>
-								<b>Dignissim a leo cum</b> <br><br>
-								A parturient adipiscing dignissim facilisi ullamcorper ut natoque vestibulum class. Mus sit ridiculus risus dignissim a leo cum adipiscing vehicula et adipiscing maecenas adipiscing senectus ultrices at pulvinar neque iaculis a viverra. Bibendum class nec sapien at cubilia dictumst torquent felis.
-
-							</div>
+							<p>
+								I'm in Tab 1.
+							</p>
+							<p>
+								Duis autem eum iriure dolor in hendrerit in vulputate velit esse molestie consequat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat.
+							</p>
+							<p>
+							</p>
 						</div>
-						<div class="tab-pane" id="tab_default_2" style="margin-top:30px;">
-							<div class="col-md-6">
-								<b>Reviews</b> <br>
-								<br>
-								There are no reviews yet.
-							</div>
-							<div class="col-md-6" style="position: relative;">
-								<b>
-								Be the first to review “Ysed rhoncus”
-								<br>
-								<br>
-								</b>
-								Your email address will not be published. Required fields are marked *
-								<br>
-								<br>
-								<div class="popo">
-								<div class="rate">your rating
-									<input type="radio" id="star5" name="rate" value="5" />
-									<label for="star5" title="text">5 stars</label>
-									<input type="radio" id="star4" name="rate" value="4" />
-									<label for="star4" title="text">4 stars</label>
-									<input type="radio" id="star3" name="rate" value="3" />
-									<label for="star3" title="text">3 stars</label>
-									<input type="radio" id="star2" name="rate" value="2" />
-									<label for="star2" title="text">2 stars</label>
-									<input type="radio" id="star1" name="rate" value="1" />
-									<label for="star1" title="text">1 star</label>
-								</div>
-								</div>
-								 <br> <br>
-								<div style="position: absolute; left: 15px;">You reviews*</div> <br>
-								<textarea rows="6" cols="60"></textarea>
-								<div style="margin-left: -15px;">
-									<div class="col-md-6">
-									Name*
-									<br>
-									<input>
-								</div>
-								<div class="col-md-6">
-									Email*
-									<br>
-									<input>
-								</div>
-								</div> <br> <br>
-								<input type="checkbox" name="vehicle1" value="Bike">Save my name, email, and website in this browser for the next time I comment.<br> <br>
-								<button type="button" class="btn btn-default">Submit</button> <br>
-							</div>
+						<div class="tab-pane" id="tab_default_2">
+							<p>
+								Howdy, I'm in Tab 2.
+							</p>
+							<p>
+								Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat. Ut wisi enim ad minim veniam, quis nostrud exerci tation.
+							</p>
+							<p>
+							</p>
 						</div>
 						<div class="tab-pane" id="tab_default_3">
 							<p>
--- 5/js/5-script.js
+++
@@ -1,9 +0,0 @@
-var swiper = new Swiper('#swiper-container2', {
-        watchSlidesProgress: true,
-                watchSlidesVisibility: true,
-                slidesPerView: 2,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
