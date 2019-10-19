84a7bac26b46f8ea79061ade5463915abe413c38
nguyenhoa99sz <nguyennguyenhoa9c@gmail.com>
2019-10-19 09:58:40 +0700

    commit

---
+++ 5/5-content.php
@@ -0,0 +1,45 @@
+<div class="type-5">
+	<div class="row">
+		<div class="col-md-7">
+			
+		</div>
+		<div class="col-md-4">
+			<div>
+				<h1 class="lll" style="font-size: 38px;color: #b57b00;line-height: 1;text-align: center;">New Collection</h1> <br>
+				<h1 style="font-size: 42px;line-height: 1.1;text-align: center">Bran New Collection Winter 2019</h1> <br>
+				<b style="text-align: center;font-family:Roboto;font-weight:400;font-style:normal">Cursus mollis justo scelerisque mollis justo placerat massa dis a consectetur libero ac mollis justo quam fames.</b>
+			</div>
+			<div class="products">
+				<div class="swiper-button-next"></div>
+    			<div class="swiper-button-prev"></div>
+				<div class="swiper-container" id="swiper-container2">
+					<div class="swiper-wrapper">
+						<div class="swiper-slide active-slider">
+						<img src="images/5.png" width="100%" class="img-responsive">
+						<div class="title-custom">Hello</div>
+						</div>
+						<div class="swiper-slide">
+						    <img src="images/5.png" width="100%" class="img-responsive">
+						    <div class="title-custom">Hello</div>
+						</div>
+						<div class="swiper-slide">
+						   <img src="images/5.png" width="100%" class="img-responsive">
+						   <div class="title-custom">Hello</div>
+						</div>
+						<div class="swiper-slide">
+						   <img src="images/5.png" width="100%" class="img-responsive">
+						   <div class="title-custom">Hello</div>
+						</div>
+						<div class="swiper-slide">
+						   <img src="images/5.png" width="100%" class="img-responsive">
+						   <div class="title-custom">Hello</div>
+						</div>
+
+					</div>
+					
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4"></div>
+	</div>
+</div>
---
+++ 5/js/5-script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('#swiper-container2', {
+        watchSlidesProgress: true,
+                watchSlidesVisibility: true,
+                slidesPerView: 2,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
