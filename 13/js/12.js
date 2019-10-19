010f45b1bba8cffd78f7b8079b85a7dea6808aa9
nguyenhoa99sz <nguyennguyenhoa9c@gmail.com>
2019-10-19 10:05:48 +0700

    Revert "commit-19"
    This reverts commit 6db7ad2a447f7e96208d94ce95a55d23ef69a642, reversing
    changes made to 84a7bac26b46f8ea79061ade5463915abe413c38.

--- 13/js/12.js
+++ 13/js/12.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function(){
    var swiper = new Swiper('.swiper-container', {
     // direction: 'vertical',
@@ -22,29 +21,4 @@ $(document).ready(function(){
       //   el: '.swiper-scrollbar',
       // },
     });
-=======
-$(document).ready(function(){
-   var swiper = new Swiper('.swiper-container', {
-    // direction: 'vertical',
-    // loop: true,
-      effect: 'cube',
-      grabCursor: true,
-      cubeEffect: {
-        shadow: false,
-        slideShadows: false,
-        shadowOffset: 20,
-        shadowScale: 0.94,
-      },
-      pagination: {
-        el: '.swiper-pagination',
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      // scrollbar: {
-      //   el: '.swiper-scrollbar',
-      // },
-    });
->>>>>>> d8b30b30d45fc8b12d7f6ac7aa4c6722006c0556
 });
