b0154472378284d911923f621c8bf57bb1aaa69e
nguyenhoa99sz <nguyennguyenhoa9c@gmail.com>
2019-10-31 11:57:00 +0700

    commit

--- module-6/6-content.php
+++ module-6/6-content.php
@@ -2,17 +2,67 @@
     <div class="container">
         <div class="row">
             <div class="topbanner">
-                <div class="col-md-1"></div>
-                <div class="col-md-9">a</div>
-                <div class="col-md-2">a</div>
+                <div class="col-md-1 socola"></div>
+                <div class="col-md-9 socola">
+                    <h2>Broser Jobs By Catagory</h2>
+                </div>
+                <div class="col-md-2 socola1"><a class="okok">Broser Full List > </a></div>
             </div>
-            <div class="midbanner">
-                <div class="col-md-2">a</div>
-                <div class="col-md-2">a</div>
-                <div class="col-md-2">a</div>
-                <div class="col-md-2">a</div>
-                <div class="col-md-2">a</div>
-                <div class="col-md-2">a</div>
+            <div class="col-md-12 midbanner">
+                <div class="col-md-2">
+                    <div class="bentrong">
+                        <i class="fa fa-tv"></i>
+                        <p class="churieng">WebSite & Soft Were</p>
+                        <p class="churieng1">Audio , Cutomor , More And More, ProGraming</p>
+                        <div class="hinhvuong">
+                        <button type="button" class="btn btn-warning">Warning</button></div>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="bentrong">
+                        <i class="fa fa-thumbs-up"></i>
+                        <p class="churieng">WebSite & Soft Were</p>
+                        <p class="churieng1">Audio , Cutomor , More And More, ProGraming</p>
+                        <div class="hinhvuong">
+                        <button type="button" class="btn btn-warning">Warning</button></div>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="bentrong">
+                        <i class="fa fa-file"></i>
+                        <p class="churieng">WebSite & Soft Were</p>
+                        <p class="churieng1">Audio , Cutomor , More And More, ProGraming</p>
+                        <div class="hinhvuong">
+                        <button type="button" class="btn btn-warning">Warning</button></div>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="bentrong">
+                        <i class="fa fa-facebook"></i>
+                        <p class="churieng">WebSite & Soft Were</p>
+                        <p class="churieng1">Audio , Cutomor , More And More, ProGraming</p>
+                        <div class="hinhvuong">
+                        <button type="button" class="btn btn-warning">Warning</button></div>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="bentrong">
+                        <i class="fa fa-car"></i>
+                        <p class="churieng">WebSite & Soft Were</p>
+                        <p class="churieng1">Audio , Cutomor , More And More, ProGraming</p>
+                        <div class="hinhvuong">
+                        <button type="button" class="btn btn-warning">Warning</button></div>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="bentrong">
+                        <i class="fa fa-phone"></i>
+                        <p class="churieng">WebSite & Soft Were</p>
+                        <p class="churieng1">Audio , Cutomor , More And More, ProGraming</p>
+                        <div class="hinhvuong">
+                        <button type="button" class="btn btn-warning">Warning</button></div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
